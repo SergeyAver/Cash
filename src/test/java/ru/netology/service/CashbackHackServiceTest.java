c9910c1631b8b07076ca9fec04d503ba7d2eb3f0
Sergey <103845930+SergeyAver@users.noreply.github.com>
2022-09-19 14:08:20 +0400

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -33,7 +33,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldCalculateIfAmountIncorrect() {
-        int amount = -50000;
+        int amount = -5000;
         int actual = service.remain(amount);
         int expected = 0;
         assertEquals(expected, actual);
